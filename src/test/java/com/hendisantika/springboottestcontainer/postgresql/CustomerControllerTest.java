d919dad76780b986daf85ee091a1a9fab770fd82
hendisantika <hendisantika@yahoo.co.id>
2023-03-07 06:27:06 +0700

    #9 - Add CustomerControllerTest ✏️ 📗 🐧

---
+++ src/test/java/com/hendisantika/springboottestcontainer/postgresql/CustomerControllerTest.java
@@ -0,0 +1,81 @@
+package com.hendisantika.springboottestcontainer.postgresql;
+
+import com.hendisantika.springboottestcontainer.entity.Customer;
+import com.hendisantika.springboottestcontainer.repository.CustomerRepository;
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+
+import java.util.List;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.collection.IsCollectionWithSize.hasSize;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-testcontainer
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 3/6/23
+ * Time: 16:22
+ * To change this template use File | Settings | File Templates.
+ */
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class CustomerControllerTest {
+
+    static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:15-alpine");
+    @Autowired
+    CustomerRepository customerRepository;
+    @LocalServerPort
+    private Integer port;
+
+    @BeforeAll
+    static void beforeAll() {
+        postgres.start();
+    }
+
+    @AfterAll
+    static void afterAll() {
+        postgres.stop();
+    }
+
+    @DynamicPropertySource
+    static void configureProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", postgres::getJdbcUrl);
+        registry.add("spring.datasource.username", postgres::getUsername);
+        registry.add("spring.datasource.password", postgres::getPassword);
+    }
+
+    @BeforeEach
+    void setUp() {
+        RestAssured.baseURI = "http://localhost:" + port;
+        customerRepository.deleteAll();
+    }
+
+    @Test
+    void shouldGetAllCustomers() {
+        List<Customer> customers = List.of(
+                new Customer(null, "Monkey D. Luffy", "luffy@onepiece.com"),
+                new Customer(null, "Roronoa Zoro", "zoro@onepiece.com")
+        );
+        customerRepository.saveAll(customers);
+
+        given()
+                .contentType(ContentType.JSON)
+                .when()
+                .get("/api/customers")
+                .then()
+                .statusCode(200)
+                .body(".", hasSize(2));
+    }
+}
