8109616e8997d375127c4afd84d47a1715b43648
hendisantika <hendisantika@yahoo.co.id>
2023-03-09 08:09:06 +0700

    #21 - Remove @Disabled 🐛✏️📗

--- src/test/java/com/hendisantika/springboottestcontainer/SpringBootTestcontainerApplicationTests.java
+++ src/test/java/com/hendisantika/springboottestcontainer/SpringBootTestcontainerApplicationTests.java
@@ -1,9 +1,7 @@
 package com.hendisantika.springboottestcontainer;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,7 +11,6 @@ import org.springframework.boot.test.web.client.TestRestTemplate;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
 
-@Disabled
 @SpringBootTest(
         properties = {
                 "management.endpoint.health.show-details=always",
